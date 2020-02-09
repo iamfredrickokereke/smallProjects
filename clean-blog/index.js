8fb6ce9ce8233dd2ef71f9b6082bf13806149076
Kelly online <kellyonline2016@gmail.com>
2020-02-09 02:41:57 +0100

    add use function on express server

--- clean-blog/index.js
+++ clean-blog/index.js
@@ -4,9 +4,11 @@ const express = require("express")
 const app = new express()
 
 
+app.use(express.static("public"))
+
 
 app.get('/', (request, response) => {
-    response.sendFile(path.resolve(__dirname, 'index.html'))
+    response.sendFile(path.resolve(__dirname, 'pages/index.html'))
 })
 
-app.listen(4000, () => { console.log("App is listening on port 4000")})
+app.listen(4000, () => { console.log("App is listening on port: 4000")})
