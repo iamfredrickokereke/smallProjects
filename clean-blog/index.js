fe7dadde8b4c8464f0eeff6e51f8e5cb7f90fcc2
Kelly online <kellyonline2016@gmail.com>
2020-02-09 02:59:42 +0100

    added more action pages on get function

--- clean-blog/index.js
+++ clean-blog/index.js
@@ -11,4 +11,16 @@ app.get('/', (request, response) => {
     response.sendFile(path.resolve(__dirname, 'pages/index.html'))
 })
 
+app.get('/about', (request, response) => {
+    response.sendFile(path.resolve(__dirname, 'pages/'))
+})
+
+app.get('/post', (request, response) => {
+    response.sendFile(path.resolve(__dirname, 'pages/'))
+})
+
+app.get('/contact', (request, response) =>{
+    response.sendFile(path.resolve(__dirname, 'pages/'))
+})
+
 app.listen(4000, () => { console.log("App is listening on port: 4000")})
