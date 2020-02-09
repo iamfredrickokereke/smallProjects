0f809ba43c3fae162d557e495042d1191fe9a834
Kelly online <kellyonline2016@gmail.com>
2020-02-09 02:39:54 +0100

    add path resolve function

--- clean-blog/index.js
+++ clean-blog/index.js
@@ -3,4 +3,10 @@ const path = require("path")
 const express = require("express")
 const app = new express()
 
+
+
+app.get('/', (request, response) => {
+    response.sendFile(path.resolve(__dirname, 'index.html'))
+})
+
 app.listen(4000, () => { console.log("App is listening on port 4000")})
