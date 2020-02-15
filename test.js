14e5018fe7e8b5bed3ce3a98178eca9b94b43bf8
Kelly online <kellyonline2016@gmail.com>
2020-02-15 23:27:49 +0100

    cleAr interval

--- test.js
+++ test.js
@@ -4,8 +4,13 @@ setTimeout(() => {
 
 let time = 0
 setInterval(() => {
-    if (time <=5)
-    {console.log("its done again")}
 
-    clearInterval(time)
+    time+=2
+
+    if (time <=5)
+    {console.log( time + "seconds has passed")}
+    else{
+
+    clearInterval() }
 }, 3000);
+
